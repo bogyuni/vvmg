1b701c26f1084daece6ef98a30d0f4ca524daf45
Q11343 <q11343@Q11343-2PN1170200003.local>
2023-11-21 17:03:45 +0900

    csv 파일 저장

--- scrape.js
+++ scrape.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
@@ -9,6 +10,7 @@ async function fetchData(url) {
 async function scrapeData() {
   const baseUrl = 'https://bogyuni.github.io/vvmg/pages/';
   const totalPages = 3; // 예시로 10페이지까지만 수집
+  const data = []; // 수집한 데이터를 저장할 배열
 
   for (let page = 1; page <= totalPages; page++) {
     const url = `${baseUrl}${page}.html`;
@@ -17,18 +19,30 @@ async function scrapeData() {
 
     // 페이지에서 필요한 정보 추출
     const title = $('title').text();
-    console.log(`Page ${page} Title: ${title}`);
 
     // 추가로 필요한 정보 수집 작업을 여기에 추가
+
+    // 수집한 데이터를 배열에 추가
+    data.push({ page, title });
 
     // 요청 간격 설정 (예: 1초)
     await delay(1000);
   }
+
+  // CSV 파일로 저장
+  saveToCSV(data, 'output.csv');
 }
 
 // 지정된 밀리초 동안 대기하는 함수
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// CSV 파일로 저장하는 함수
+function saveToCSV(data, filename) {
+  const csvContent = data.map(row => Object.values(row).join(',')).join('\n');
+  fs.writeFileSync(filename, csvContent, 'utf-8');
+  console.log(`Data has been saved to ${filename}`);
 }
 
 scrapeData();

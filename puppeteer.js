71522da17b76b5e4cf11297df112fb59ccb2eb8e
bogyuni <bogyuni@hotmail.com>
2023-11-23 00:39:40 +0900

    품번 data 정제완료

--- puppeteer.js
+++ puppeteer.js
@@ -3,74 +3,80 @@ const fs = require('fs');
 
 async function scrapeData() {
 	const browser = await puppeteer.launch({
-    headless: 'new', // "new"로 설정
-  });
+		headless: 'new', // "new"로 설정
+	});
 	const page = await browser.newPage();
+	// const baseUrl = 'https://www.avdbs.com/menu/actor.php?actor_idx=';
+	const baseUrl = 'https://www.avdbs.com/menu/dvd.php?dvd_idx=';
+	const url = 'https://www.avdbs.com'; // 대상 URL로 변경
+	const urlMin = 1;
+	const urlMax = 100;
+	const pageUrls = [];
+	for (let i = urlMin; i <= urlMax; i++) {
+		pageUrls.push(baseUrl+i);
+	}
+	const data = [];
+	let csvData = '';
+	let resultData = '';
 
 	try {
-		// const baseUrl = 'https://www.avdbs.com/menu/actor.php?actor_idx=';
-		// /menu/dvd.php?dvd_idx=761931
-		const baseUrl = 'https://www.avdbs.com/menu/dvd.php?dvd_idx=';
-		const urlCnt = 10;
-		const pageUrls = [];
-		for (let i = 1; i < urlCnt; i++) {
-			pageUrls.push(baseUrl+i);
+		const response = await page.goto(url);
+		if (!response.ok()) {
+			throw new Error(`${response.status()} ${response.statusText()} at ${url}`);
 		}
-		const data = [];
-    const url = 'https://www.avdbs.com'; // 대상 URL로 변경
-    const response = await page.goto(url);
 
-    if (!response.ok()) {
-      throw new Error(`${response.status()} ${response.statusText()} at ${url}`);
-    }
-
-		let csvData = '';
-		let resultData = '';
-
-    // 나머지 코드...
 		for (const url of pageUrls) {
 			let timeoutOccurred = false;
 			try {
-				await page.goto(url, { timeout: 1000 }); // 타임아웃을 1초로 설정
+				await page.goto(url, { timeout: 500 }); // 타임아웃을 1초로 설정
 				// 원하는 HTML 클래스 선택자
-				const name_en = '.title';
-				// const name_ko = '.inner_name_kr';
-				// const name_ko = '.snum';
+				const selectedData = '.title';
+				const profileDetail = '.profile_detail';
 				// 페이지 내에서 해당 선택자에 일치하는 요소가 있는지 확인
-				const isElementExist = await page.$(name_en);
-				// const isSnum = await page.$(name_ko);
-				// console.log(isElementExist, isSnum);
-				// return;
+				const isElementExist = await page.$(selectedData);
 
 				if (isElementExist) {
 					// 선택자에 일치하는 내용을 추출
-					const customText = await page.$eval(name_en, element => element.textContent);
-					// const elementContentName_en = await page.$eval(name_en, element => element.textContent);
-					const elementContentName_en = customText.replace(/\s/g, '').replace(/\//g, ',');
-					
-					// const elementContentName_ko = await page.$eval(name_ko[0], element => element.textContent);
-					const reMsg = `Content of the selected element on ${url}:, ${elementContentName_en}`;
-					console.log(reMsg);
+					const customData = await page.$eval(selectedData, element => element.textContent);
+					const customProfile = await page.$eval(profileDetail, element => element.textContent);
+
+					// console.log(customProfile);
+
+
+					const description = customData.replace(/\s/g, '').replace(/\//g, ',');
+					const profile = customProfile
+													.replace(/\n/g, ',')
+													.replace(/\s/g, '')
+													.replace(':,', ':')
+													.replace(',출시:', '')
+													.replace('출연:', '')
+													.replace('제작사:', '')
+													.replace('레이블:', '')
+													.replace('시리즈:', '')
+													.replace('감독:', '')
+													.replace('재생시간:', '');
+					// const profile = customProfile;
+
+					const reMsg = `Content of the selected element on ${url}:, ${description}`;
+					// console.log(reMsg);
 					resultData += reMsg+'\n';
 					// 데이터 배열에 추가
 					data.push({
 						url: url,
-						nameEn: elementContentName_en,
-						// nameKo: elementContentName_ko,
+						desc: description,
+						prof: profile,
 					});
 				} else {
-					// console.error(`No element found for name_en "${name_en}" on ${url}`);
-					const reMsg = `No element found for name_en "${name_en}" on ${url}`;
-					console.error(reMsg);
+					const reMsg = `No element found for selectedData "${selectedData}" on ${url}`;
+					// console.error(reMsg);
 					resultData += reMsg+'\n';
 				}
 				// 데이터를 CSV 문자열로 변환
-				// csvData = data.map(item => `${item.url},${item.nameEn},${item.nameKo}`).join('\n');
-				csvData = data.map(item => `${item.url},${item.nameEn}`).join('\n');
+				csvData = data.map(item => `${item.url.split(baseUrl)[1]},${item.desc},${item.prof}`).join('\n');
 			} catch (error) {
 				if (error.message.includes('Navigation timeout')) {
 					const reMsg = `Timeout error occurred for URL: ${page.url()}`;
-					console.error(reMsg);
+					// console.error(reMsg);
 					resultData += reMsg+'\n';
 					timeoutOccurred = true;
 				} else {
@@ -79,18 +85,18 @@ async function scrapeData() {
 			}
 
 			if (timeoutOccurred) {
-        // 타임아웃이 발생하면 현재 URL을 건너뛰고 다음 URL로 진행
-        continue;
-      }
+				// 타임아웃이 발생하면 현재 URL을 건너뛰고 다음 URL로 진행
+				continue;
+			}
 		}
 
 		// CSV 파일로 저장
-		// fs.writeFileSync('output.csv', 'url,name_en,name_ko\n' + csvData, 'utf-8');
-		fs.writeFileSync('output.csv', 'url,p_num,name_en\n' + csvData, 'utf-8');
+		// fs.writeFileSync('./data/av_prduct.csv', 'url_id, 품번, 한글명, 일어명, 영어명, profile\n' + csvData, 'utf-8');
+		fs.writeFileSync('./data/av_prduct.csv', 'url_id, 품번, 한글명, 일어명, 영어명, 출시일, 출연, 제작사, 레이블, 시리즈, 감독, 재생시간\n' + csvData, 'utf-8');
 		fs.writeFileSync('resultLog.csv', 'url,data\n' + resultData, 'utf-8');
-  } finally {
-    await browser.close();
-  }
+	} finally {
+		await browser.close();
+	}
 }
 
 scrapeData();

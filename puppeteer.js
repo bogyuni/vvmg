e73a788b04a8dd5f16d3aa0dc2a3d266f3424365
Q11343 <q11343@Q11343-2PN1170200003.local>
2023-11-22 15:05:05 +0900

    품번페이지로 자료 추출중

--- puppeteer.js
+++ puppeteer.js
@@ -8,8 +8,10 @@ async function scrapeData() {
 	const page = await browser.newPage();
 
 	try {
-		const baseUrl = 'https://www.avdbs.com/menu/actor.php?actor_idx=';
-		const urlCnt = 20;
+		// const baseUrl = 'https://www.avdbs.com/menu/actor.php?actor_idx=';
+		// /menu/dvd.php?dvd_idx=761931
+		const baseUrl = 'https://www.avdbs.com/menu/dvd.php?dvd_idx=';
+		const urlCnt = 10;
 		const pageUrls = [];
 		for (let i = 1; i < urlCnt; i++) {
 			pageUrls.push(baseUrl+i);
@@ -31,15 +33,22 @@ async function scrapeData() {
 			try {
 				await page.goto(url, { timeout: 1000 }); // 타임아웃을 1초로 설정
 				// 원하는 HTML 클래스 선택자
-				const name_en = '.inner_name_en';
-				const name_ko = '.inner_name_kr';
+				const name_en = '.title';
+				// const name_ko = '.inner_name_kr';
+				// const name_ko = '.snum';
 				// 페이지 내에서 해당 선택자에 일치하는 요소가 있는지 확인
 				const isElementExist = await page.$(name_en);
+				// const isSnum = await page.$(name_ko);
+				// console.log(isElementExist, isSnum);
+				// return;
 
 				if (isElementExist) {
 					// 선택자에 일치하는 내용을 추출
-					const elementContentName_en = await page.$eval(name_en, element => element.textContent);
-					const elementContentName_ko = await page.$eval(name_ko, element => element.textContent);
+					const customText = await page.$eval(name_en, element => element.textContent);
+					// const elementContentName_en = await page.$eval(name_en, element => element.textContent);
+					const elementContentName_en = customText.replace(/\s/g, '').replace(/\//g, ',');
+					
+					// const elementContentName_ko = await page.$eval(name_ko[0], element => element.textContent);
 					const reMsg = `Content of the selected element on ${url}:, ${elementContentName_en}`;
 					console.log(reMsg);
 					resultData += reMsg+'\n';
@@ -47,13 +56,17 @@ async function scrapeData() {
 					data.push({
 						url: url,
 						nameEn: elementContentName_en,
-						nameKo: elementContentName_ko,
+						// nameKo: elementContentName_ko,
 					});
 				} else {
-					console.error(`No element found for name_en "${name_en}" on ${url}`);
+					// console.error(`No element found for name_en "${name_en}" on ${url}`);
+					const reMsg = `No element found for name_en "${name_en}" on ${url}`;
+					console.error(reMsg);
+					resultData += reMsg+'\n';
 				}
 				// 데이터를 CSV 문자열로 변환
-				csvData = data.map(item => `${item.url},${item.nameEn},${item.nameKo}`).join('\n');
+				// csvData = data.map(item => `${item.url},${item.nameEn},${item.nameKo}`).join('\n');
+				csvData = data.map(item => `${item.url},${item.nameEn}`).join('\n');
 			} catch (error) {
 				if (error.message.includes('Navigation timeout')) {
 					const reMsg = `Timeout error occurred for URL: ${page.url()}`;
@@ -72,7 +85,8 @@ async function scrapeData() {
 		}
 
 		// CSV 파일로 저장
-		fs.writeFileSync('output.csv', 'url,name_en,name_ko\n' + csvData, 'utf-8');
+		// fs.writeFileSync('output.csv', 'url,name_en,name_ko\n' + csvData, 'utf-8');
+		fs.writeFileSync('output.csv', 'url,p_num,name_en\n' + csvData, 'utf-8');
 		fs.writeFileSync('resultLog.csv', 'url,data\n' + resultData, 'utf-8');
   } finally {
     await browser.close();
